1fc8dbabe231fdbb0af2bebebeb2819a4195a30d
Lada Petkevich <ladap1999@gmail.com>
2022-11-07 12:55:29 +0300

    add throws in method, change tabs

--- src/com/homework7/Addition.java
+++ src/com/homework7/Addition.java
@@ -3,7 +3,7 @@ package com.homework7;
 public class Addition implements Calculator {
     @Override
     public void action(double a, double b) {
-     double sum = a + b;
+        double sum = a + b;
         System.out.println("The sum is " + sum);
     }
 }
